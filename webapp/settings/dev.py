fef83bd84233fe2e0d5a4cd5ec0af61813d6b2de
Nick Moreton <nickmoreton@me.com>
2023-08-19 16:43:05 +0100

    Initial Wagtail setup
    move files and folders around and added .env

---
+++ webapp/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-1r93dd+db8)#&g_%2o5)78zh!#rop*eclqgv$y)my@k2p&ra*)"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
